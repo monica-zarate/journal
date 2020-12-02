f564faa113f5d40d6c0815e5e1a09fbda639ea9c
Monica Zarate <monicazaratecortes@gmail.com>
2020-12-01 18:58:04 -0800

    added classNames to header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles/main.css";
 
 function Header (){
     return(
         
-    <div><h1>Header Component</h1>
-    </div>
-
-    
+    <div className="header">
+        <Link className="header__title-link" to={`/`}>
+        <h1 className="header__ title">True to Life</h1>
+        </Link>
+    <ul className="header__nav">
+        <li className="header__li">
+            <Link className="header__link" to={`/about`}>About
+            </Link>
+        </li>
+                <li className="header__li">
+            <Link className="header__link" to={`/blog_menu`}>Blog Menu
+            </Link>
+        </li>
+    </ul>
+    </div>   
     );
 }
 
