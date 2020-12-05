5392fa26d87a83f7f1e5f3aa544ebcf1a9720929
Monica Zarate <monicazaratecortes@gmail.com>
2020-12-04 19:56:50 -0800

    Basic styles for AboutLanding Component.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Header from "./components/Header";
 import Landing from "./components/Landing";
 import About from "./components/About";
 import BlogMenu from "./components/BlogMenu";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+//import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  library.add(fab);
   return (
     <div className="App">
       <Header />
