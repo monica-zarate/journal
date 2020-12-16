ca7c10903df24168fdb410b2fd08f7bc68277908
Monica Zarate <monicazaratecortes@gmail.com>
2020-12-16 13:22:11 -0800

    Created model json files. Express and npm set-up on server.js

---
+++ server/server.js
@@ -0,0 +1,14 @@
+// Express and npm set-up
+const express = require("express");
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+
+// Const App will be using Express method
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// App listening on local host
+app.listen(5000, console.log("app is listening at http://localhost:5000"));
