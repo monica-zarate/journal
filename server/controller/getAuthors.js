1bcf3e6c82cf653d66b8404272ce60db70f7ddfa
Monica Zarate <monicazaratecortes@gmail.com>
2020-12-18 13:41:05 -0800

    Updated getAuthors controller to read authors data from db. Tested with Postman. Deleted authors.json from model folder.

--- server/controller/getAuthors.js
+++ server/controller/getAuthors.js
@@ -1,8 +1,19 @@
-const fs = require("fs");
+const mysql = require("sync-mysql");
 
 const getAuthors = () => {
-  const authorsData = JSON.parse(fs.readFileSync("./model/authors.json"));
+  const password = process.env.password;
+  const connection = new mysql({
+    host: "localhost",
+    user: "root",
+    password: password,
+    database: "my_journal",
+  });
 
+  let authorsData = [];
+  let rows = connection.query("SELECT id, name, bio FROM authors");
+  for (i = 0; i < rows.length; i++) {
+    authorsData.push(rows[i]);
+  }
   return authorsData;
 };
 
