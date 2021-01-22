384b4bfc4ea07ce4a3a496f78a40d6daffe28ef2
Monica Zarate <monicazaratecortes@gmail.com>
2021-01-21 20:05:04 -0800

    Updated DB & Controllers to getAuthors new columns. Updated Axios calls in About & About Landing Components. Styled responsive.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import "./styles/main.css";
-import about from "../assets/imgs/about.png";
+// import about from "../assets/imgs/about.png";
 import Axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-//Posts API
-const postsAPI = "http://localhost:5000/entries";
+//Authors API
+const postsAPI = "http://localhost:5000/authors";
 
 function About (){
 
     const getContent = ()=>{Axios.get(postsAPI)
         .then((response)=>{
             let aboutContent = document.querySelector(".about__content");
-            let contentResponse = response.data[0].content;
+            let contentResponse = response.data[0].about;
             aboutContent.innerHTML = contentResponse;
+            let aboutImg = document.querySelector(".about__img");
+            let imgResponse = response.data[0].thumb;
+            aboutImg.src = imgResponse;
         })
         .catch((error)=>{
         console.log(error)
@@ -22,12 +25,11 @@ function About (){
 
     return(
     <div className="about">
-        <h1 className="about__title">Hello there!</h1>
-        <h2 className="about__subtitle">Welcome to Monica's Solstice</h2>
-        <img className="about__img" src={about} alt=""/>
+        <h1 className="about__title">Everyone is Welcome! <span>🍃</span></h1>
+        <img className="about__img" src={getContent()} alt=""/>
         <p className="about__content">{getContent()}</p>
-        <a className="about-l__social-link" href="https://www.instagram.com/monica.solstice/">
-            <button className="about-l__social"><FontAwesomeIcon icon={['fab','instagram']} size="3x"/></button>
+        <a className="about__social-link" href="https://www.instagram.com/monica.solstice/" target="_blank" rel="noopener noreferrer">
+            <button className="about__social"><FontAwesomeIcon icon={['fab','instagram']} size="3x"/></button>
         </a>
     </div>
     );
--- client/src/components/AboutLanding.jsx
+++ client/src/components/AboutLanding.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import "./styles/main.css";
-import author from "../assets/imgs/author_icon.jpg";
+// import author from "../assets/imgs/author_icon.jpg";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Axios from "axios";
+
+//Authors API
+const authorsAPI = "http://localhost:5000/authors/";
 
 function AboutLanding(){
+    const getAuthor = ()=>{
+        Axios.get(authorsAPI).then((response)=>{
+            let aboutDescription = document.querySelector(".about-l__description");
+            let contentResponse = response.data[0].landing;
+            aboutDescription.innerHTML = contentResponse;
+            let aboutImg = document.querySelector(".about-l__icon");
+            let imgResponse = response.data[0].thumb;
+            aboutImg.src = imgResponse;
+        }).catch((error)=>{
+            console.log(error)
+        })
+    };
+
     return(
         <div className="about-l">
             <h1 className="about-l__title">Hello and welcome!</h1>
             <h2 className="about-l__subtitle">My name is Monica</h2>
-            <img className="about-l__icon" src={author} alt=""/>
-            <p className="about-l__description">Monica's Solstice was born to serve as a Pensieve, a place where I can keep a record of experiences and memories to come back to and share with others. I'm happy you're here! Take a look around, come along with me and my camera!</p>
+            <img className="about-l__icon" src={getAuthor()} alt=""/>
+            <p className="about-l__description">{getAuthor()}</p>
             <Link to={`/about`}className="about-l__link">
             <button className="about-l__btn">About me</button>
             </Link>
-            <a className="about-l__social-link" href="https://www.instagram.com/monica.solstice/">
+            <a className="about-l__social-link" href="https://www.instagram.com/monica.solstice/" target="_blank" rel="noopener noreferrer">
             <button className="about-l__social"><FontAwesomeIcon icon={['fab','instagram']} size="3x"/></button></a>
         </div>
     )
