9bdad353b02fadfaff8ae6af5c0ed67286ce120a
Monica Zarate <monicazaratecortes@gmail.com>
2021-01-29 16:30:20 -0800

    Updated query on getEntry Controller to retrieve keywords names.

--- server/controller/getEntry.js
+++ server/controller/getEntry.js
@@ -13,10 +13,15 @@ const getEntry = (path) => {
   });
 
   let entryData = [];
-  let rows = connection.query(`SELECT entry.*, author.name  FROM 
-                              entries as entry
-                              join authors as author
-                              on entry.author_id = author.id where entry.path = '${path}'`);
+  let rows = connection.query(
+    `select entry.*, key_1.title as title1, key_2.title as title2, key_3.title as title3, author.name as name
+    from entries as entry
+    join keywords as key_1 on entry.keyword1_id = key_1.id
+    left join keywords as key_2 on entry.keyword2_id = key_2.id
+    left join keywords as Key_3 on entry.keyword3_id = key_3.id
+    join authors as author on entry.author_id = author.id
+    where entry.path = '${path}'`
+  );
   entryData.push(rows[0]);
 
   return entryData;
